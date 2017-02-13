09087e0e3f7bd61fc7c017832972ec4ccd3063af
Malhardixit <malharsoham@gmail.com>
2017-02-13 16:03:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,12 +8,63 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var articleOne = {
+    title:'Article One|Malhar Dixit',
+    heading:'Article One',
+    date:'Feb 13 2017',
+    content:
+    `<p>
+    This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+        
+         <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>
+        
+         <p>
+            This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+        </p>`
+};
+function createTemplate (data) {
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
 
+var htmltemplate = `
+<html>
+    
+<head>
+    <title>
+       ${title}
+        </title>
+        <meta name="viewport" content="widht=device-width,initial-scale= 1"/>
+     <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        
+    <div>
+        <a href='/'>Home</a>
+    </div>
+    <hr/>
+    <h3>
+       ${heading}
+    </h3>
+    <div>
+       ${date}
+    </div>
+    <div>
+      ${content}
+    </div>
+</div>     
+</html>
+`;
+return htmltemplate;
 
-
-
+}
 app.get('/article-one', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createtemplate(ArticleOne));
 }); 
   
 app.get('/article-two', function (req, res){
