3c18cd9750149451afd49d241f1f423247c505d1
Malhardixit <malharsoham@gmail.com>
2017-02-13 16:54:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,8 +88,8 @@ return htmltemplate;
 app.get(':articleName', function (req, res){
     // articleName == article-one
     // articles[articleName] == {} content objct for article one
-    var articleName= req.params.articleName;
-     res.send(createtemplate(articles[articleName]));
+    var articleName = req.params.articleName;
+     res.send(createTemplate(articles[articleName]));
 }); 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
