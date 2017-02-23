e1b8a1e0fb15e6ebb25bc8c54cc8bcdf705c1304
Malhardixit <malharsoham@gmail.com>
2017-02-23 22:01:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 // Change the text of the main-text div
 var element= document.getElementById('main-text');
-Element.innerHTML="Malhar Dixit";
+Element.innerHTML="New Value";
