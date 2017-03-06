7a6fdff6d9e668fc8fbad7ba17f8959140a642fd
Malhardixit <malharsoham@gmail.com>
2017-03-06 22:53:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var counter=0;
+  app.get('/counter',function (req, res){
+   counter = counter+1;
+   res.send(counter.tostring());
+  });
 var articles={
 'article-one' : {
     title:'Article-One|Malhar Dixit',
