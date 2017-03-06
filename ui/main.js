c4d2e46d64f0a552258d5dec7bb4b84997a54f8e
Malhardixit <malharsoham@gmail.com>
2017-03-06 23:10:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,13 @@
-console.log('Loaded!');
-// Change the text of the main-text div
-var element= document.getElementById ('main-text'
-);
-element.innerHtml= 'New Value';
+//Counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick= function(){
+  // Make a request to the counter endpoint
+  
+  // Capture a response and store it in a variable
+  
+  // Render the variable in the correct span
+  counter = counter= + 1;
+  var span = document.getElementById('count');
+  span.innerHTML= counter.toString();
+};
